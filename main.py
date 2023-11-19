63275ac53364e67ec06611c4448c4697d6721a43
Admin <felcome36@gmail.com>
2023-11-20 00:33:27 +0200

    Changed lists to tuples where it's needed.

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ It could take time to calculate huge numbers ( > 200**2 ).
 In my interpretation it works like this:
 1. Get an input ( *input* );
 2. Create an array with *input* arrays, containing *input* items (0);
-3. Put (1) in array[int( *input* / 2 ) + 1][int( *input* / 2) ] as it's center of entire array;
+3. Put (1) in array(int( *input* / 2 ) + 1)(int( *input* / 2) ) as it's center of entire array;
 4. Put key numbers in diagonal down-right;
-5. Moving from array[int( *input* / 2 ) + 2] upside down, right-side left starting of diagonally placed numbers and symmetrically stops;
+5. Moving from array(int( *input* / 2 ) + 2) upside down, right-side left starting of diagonally placed numbers and symmetrically stops;
 6. Changing direction clockwise; 
 7. Calculating prime numbers in given range;
 8. Replasing numbers in array with 1 for prime numbers and 0 if not;
@@ -85,17 +85,17 @@ for i in range(np.add(num, 1)):
 prime.sort()
 
 perc = 0
-img = Image.new(mode='RGB', size=[np.multiply(sqrn, 2), np.multiply(sqrn, 2)], color='black')
+img = Image.new(mode='RGB', size=(np.multiply(sqrn, 2), np.multiply(sqrn, 2)), color='black')
 for j, i in enumerate(array):
     if perc + 5 <= round(j / len(array) * 100, 2):
         print(f'Drawing {round(j / len(array) * 100, 2)}%')
         perc = round(j / len(array) * 100, 2)
     for l, k in enumerate(i):
         if k in prime:
-            img.putpixel([np.multiply(l, 2), np.multiply(j, 2)], (255, 255, 255, 0))
-            img.putpixel([np.add(np.multiply(l, 2), 1), np.multiply(j, 2)], (255, 255, 255, 0))
-            img.putpixel([np.multiply(l, 2), np.add(np.multiply(j, 2), 1)], (255, 255, 255, 0))
-            img.putpixel([np.add(np.multiply(l, 2), 1), np.add(np.multiply(j, 2), 1)], (255, 255, 255, 0))
+            img.putpixel((np.multiply(l, 2), np.multiply(j, 2)), (255, 255, 255, 0))
+            img.putpixel((np.add(np.multiply(l, 2), 1), np.multiply(j, 2)), (255, 255, 255, 0))
+            img.putpixel((np.multiply(l, 2), np.add(np.multiply(j, 2), 1)), (255, 255, 255, 0))
+            img.putpixel((np.add(np.multiply(l, 2), 1), np.add(np.multiply(j, 2), 1)), (255, 255, 255, 0))
 img.save(f'sop_{num}.png')
 print('Done')
 print(f'File "sop_{num}.png" created.')
